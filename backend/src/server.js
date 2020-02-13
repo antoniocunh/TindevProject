1e88e7de579ddef57a24a2cec813ce5b6466b8b5
António Cunha <antonio.cunh@ua.pt>
2020-02-13 10:54:41 +0000

    Adicionar o mongoose e o nodemon

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,3 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const routes = require('./routes.js');
 
 const server = express();
+
+mongoose.connect('mongodb+srv://teste:teste@cluster0-urbu7.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
+
+server.use(express.json());
+server.use(routes);
+
+server.listen(3333);
